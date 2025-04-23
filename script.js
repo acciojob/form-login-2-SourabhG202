822d4971c820493a65e665690c9fe295c26d20b4
Sourabh G <154436409+SourabhG202@users.noreply.github.com>
2025-04-23 14:20:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-//your JS code here. If required.
-const form=document.getElementById('loginForm');
+// your JS code here
+const form = document.getElementById('loginForm');
 
-form.addEventListner('submit',finction(e){
+form.addEventListener('submit', function(e) {
   e.preventDefault();
   
-  const firstName=document.getElementById('firstName');
-  const lastName=document.getElementById('lastName');
-  const phoneNumber=document.getElementById('phoneNumber');
-  const emailId=document.getElementById('emailId');
+  const firstName = document.getElementById('firstName').value;
+  const lastName = document.getElementById('lastName').value;
+  const phoneNumber = document.getElementById('phoneNumber').value;
+  const emailId = document.getElementById('emailId').value;
   
   alert(
-    `First Name\t Last Name\t Phone Number\t Email ID\n` +
-    `${firstName}\t ${lastName}\t ${phoneNumber}\t ${emailId}`
-    );
+    `First Name: ${firstName}\n` +
+    `Last Name: ${lastName}\n` +
+    `Phone Number: ${phoneNumber}\n` +
+    `Email ID: ${emailId}`
+  );
 });
